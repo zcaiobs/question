58478c25b1ac08a7280a69603f50c5ec81c6b5ce
zcaiobs <zcaiobernardodasilva@gmail.com>
2025-03-02 13:05:33 -0300

    test: ajuste

--- src/test/kotlin/com/app/question/QuestionControllerTest.kt
+++ src/test/kotlin/com/app/question/QuestionControllerTest.kt
@@ -11,12 +11,15 @@ import org.mockito.Mock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.cache.DefaultCacheAwareContextLoaderDelegate
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.presigner.S3Presigner
 import java.util.UUID
 
 @Transactional
@@ -26,6 +29,12 @@ class QuestionControllerTest {
 
     @Mock
     lateinit var awsMock: DefaultCacheAwareContextLoaderDelegate
+
+    @Mock
+    lateinit var s3Client: S3Client
+
+    @Mock
+    lateinit var s3Presigner: S3Presigner
 
     @InjectMocks
     lateinit var questionService: QuestionService
